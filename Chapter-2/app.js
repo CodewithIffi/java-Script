21dd3c4eabc51010caf9bd0851c8659ad5d8043f
unknown <ifficodder@gmail.com>
2024-02-24 21:27:19 +0500

    chapter-2

---
+++ Chapter-2/app.js
@@ -0,0 +1,61 @@
+// 1 variable for username 
+
+var username;
+
+// 2 string variable for my name 
+
+var myName = "Irfan";
+
+
+// 3 script for message 
+
+var message;
+message = "Hello World";
+alert(message);
+
+
+// 4 script for student biodata 
+
+var studentName = 'Irfan';
+var studentAge = '25';
+var studentGrade ='A';
+var studentBioData = 'Name: ' + studentName + '/nAge: ' + studentAge + '/nGrade: ' + studentGrade; 
+
+alert(studentBioData);
+
+// 5 ons js variable alert 
+
+
+
+
+// 6 Declare a variable of email 
+
+
+var email = "My email address is ik@gmail.com";
+alert("My email address is: ik@gmail.com");
+
+
+// 7 Declare a variable of Book 
+
+var Book = "A smarter way to learn javaScript";
+alert("I am currently reading the book: " + Book);
+
+
+// 8 script to display in browser through JS
+
+
+
+var Book = "content through JavaScript";
+document.write("Yah! I can write HTML " + Book);
+
+
+// 9  Following string to show in alert box 
+
+
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+
+
+
